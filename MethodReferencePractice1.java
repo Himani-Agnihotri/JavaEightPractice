0161e625ca536324b026d2185b707adfa5ca7e33
Himani Agnihotri <himani.a2395@gmail.com>
2024-10-12 16:07:55 +0530

    Added first class for java 8 method reference feature

--- MethodReferencePractice1.java
+++ MethodReferencePractice1.java
@@ -1,2 +1,11 @@
 public class MethodReferencePractice1 {
+
+    //Classname::methodName
+
+    public static void main(String args[]){
+      System.out.println(MethodReferencePractice1.compare(10,20));
+    }
+    public static int compare(Integer x, Integer y){
+        return Integer.compare(x,y);
+    }
 }
